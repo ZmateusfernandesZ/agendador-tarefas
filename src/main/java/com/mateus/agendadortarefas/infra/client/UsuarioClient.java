bd07bbdf6e80bc933b1db7fb5442db352fd10409
Mateus <mateusfernandes0927@gmail.com>
2026-08-29 23:26:51 -0300

    fix:adicionado config de seguranca

--- src/main/java/com/mateus/agendadortarefas/infra/client/UsuarioClient.java
+++ src/main/java/com/mateus/agendadortarefas/infra/client/UsuarioClient.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "usuario", url = "${usuario.url}")
+@FeignClient(name = "usuario", url = "${usuario.url}/")
 public interface UsuarioClient {
 
-    @GetMapping
+    @GetMapping("/usuario")
     UsuarioDTO buscaUsuarioPorEmail(@RequestParam("email") String email, @RequestHeader("Authorization") String token);
 
 }
